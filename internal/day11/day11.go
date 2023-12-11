38085efe94ed243bc89e0cc7630a8fbd8b1b9ea5
Adam Fox <adam@foxy.id.au>
2023-12-11 17:16:47 +1030

    day11: part2

--- internal/day11/day11.go
+++ internal/day11/day11.go
@@ -2,97 +2,94 @@ package day11
 
 import (
 	"bufio"
-	"fmt"
+	"log/slog"
 
 	"github.com/foax/aoc2023/internal/util"
 )
 
-type galaxy [][]rune
-
-func printGalaxy(g galaxy) {
-	for _, line := range g {
-		fmt.Println(string(line))
-	}
+type galaxy struct {
+	coords      [][2]int
+	emptyRow    map[int]bool
+	emptyCol    map[int]bool
+	emptyFactor int
 }
 
-func loadGalaxy(input []string) galaxy {
-	g := make(galaxy, len(input))
-	for i, line := range input {
-		g[i] = []rune(line)
-	}
-	return g
-}
+func loadGalaxy(input []string, f int) galaxy {
+	g := galaxy{emptyFactor: f}
+	g.emptyRow = make(map[int]bool)
+	g.emptyCol = make(map[int]bool)
 
-func expandGalaxy(g galaxy) (galaxy, [][2]int) {
 	galRows := make(map[int]bool)
 	galCols := make(map[int]bool)
-
-	// check how big this new galaxy needs to be
-
-	for i, line := range g {
-		for j, x := range line {
-			if x == '#' {
+	for i, line := range input {
+		for j, star := range line {
+			if star == '#' {
+				g.coords = append(g.coords, [2]int{i, j})
 				galRows[i] = true
 				galCols[j] = true
 			}
 		}
 	}
 
-	fmt.Println(galRows, galCols)
-
-	newGalaxy := make(galaxy, 0)
-	galaxyCoords := make([][2]int, 0)
-	lineOfDots := make([]rune, 2*len(g[0])-len(galCols))
-	for i := range lineOfDots {
-		lineOfDots[i] = '.'
-	}
-
-	for i, line := range g {
+	for i := 0; i < len(input); i++ {
 		if !galRows[i] {
-			newGalaxy = append(newGalaxy, lineOfDots, lineOfDots)
-		} else {
-			newLine := make([]rune, 2*len(g[0])-len(galCols))
-			for j, k := 0, 0; k < len(newLine); j++ {
-				if !galCols[j] {
-					newLine[k] = '.'
-					k++
-					newLine[k] = '.'
-				} else {
-					newLine[k] = line[j]
-					if newLine[k] == '#' {
-						galaxyCoords = append(galaxyCoords, [2]int{len(newGalaxy), k})
-					}
-				}
-				k++
-			}
-			newGalaxy = append(newGalaxy, newLine)
+			g.emptyRow[i] = true
 		}
 	}
-	return newGalaxy, galaxyCoords
+	for j := 0; j < len(input[0]); j++ {
+		if !galCols[j] {
+			g.emptyCol[j] = true
+		}
+	}
+	return g
 }
 
-func galaxySteps(x, y [2]int) int {
-	return util.IntAbs(x[0]-y[0]) + util.IntAbs(x[1]-y[1])
+func galaxySteps(g galaxy, x, y [2]int) int {
+
+	steps := util.IntAbs(x[0]-y[0]) + util.IntAbs(x[1]-y[1])
+	for _, data := range []struct {
+		idx   int
+		empty map[int]bool
+	}{
+		{0, g.emptyRow},
+		{1, g.emptyCol},
+	} {
+
+		step := 0
+		if x[data.idx] > y[data.idx] {
+			step = -1
+		} else if x[data.idx] < y[data.idx] {
+			step = 1
+		} else {
+			step = 0
+		}
+
+		for a := x[data.idx] + step; a != y[data.idx]; a += step {
+			if data.empty[a] {
+				steps += g.emptyFactor - 1
+			}
+		}
+	}
+	return steps
+
+}
+
+func partHandler(input []string, f int) int {
+	g := loadGalaxy(input, f)
+	total := 0
+	for i, x := range g.coords {
+		for _, y := range g.coords[i+1:] {
+			steps := galaxySteps(g, x, y)
+			total += steps
+		}
+	}
+	return total
 }
 
 func Execute(scanner *bufio.Scanner) {
 	input := util.ReadInput(scanner)
-	g := loadGalaxy(input)
-	printGalaxy(g)
-	fmt.Println("---")
-	g, coords := expandGalaxy(g)
-	printGalaxy(g)
-	fmt.Println(coords)
+	part1 := partHandler(input, 2)
+	part2 := partHandler(input, 1000000)
 
-	total := 0
-	for i, x := range coords {
-		for _, y := range coords[i+1:] {
-			steps := galaxySteps(x, y)
-			fmt.Printf("Steps from %v to %v: %d\n", x, y, steps)
-			total += steps
-		}
-	}
-	fmt.Println(total)
-
-	// slog.Info("Results", "part1", part1Total, "part2", part2Total)
+	slog.Info("Results", "part1", part1, "part2", part2)
 }
